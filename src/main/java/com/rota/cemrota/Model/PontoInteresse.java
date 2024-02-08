665388ba2414c23a34a1000a3300f8c6c2c35fa0
Samuel Lúcio <102866875+samucalucio1997@users.noreply.github.com>
2024-02-08 17:07:47 +0000

    PontoInteresse model

--- src/main/java/com/rota/cemrota/Model/PontoInteresse.java
+++ src/main/java/com/rota/cemrota/Model/PontoInteresse.java
@@ -1,8 +1,24 @@
 package com.rota.cemrota.Model;
 
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class PontoInteresse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private Double longitude;
+    private Double latitude;
+    private String turnos;
+    private String dias;
     
+    @OneToOne(fetch = FetchType.EAGER)
+    private Endereco endereco;
 }
---
+++ src/main/java/com/rota/cemrota/Model/Servico.java
@@ -0,0 +1,6 @@
+package com.rota.cemrota.Model;
+
+
+public enum Servico {
+   Hospedagem, Venda, Evento, Ponto_Turistico
+}
